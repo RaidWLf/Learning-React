192e703afda8a63b7c6af6f94ca1956bac76d15d
RaidWLf <ambuj.kumar.7903@gmail.com>
2023-09-05 23:15:32 +0530

    Made a Currency Converter using API to get Real Time Data

---
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+const useCurrencyInfo = (currency) => {
+  const [currencyInfo, setCurrencyInfo] = useState({});
+
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+    )
+      .then((response) => response.json())
+      .then((data) => setCurrencyInfo(data[currency]));
+  }, [currency]);
+
+  return currencyInfo;
+};
+
+export default useCurrencyInfo;
